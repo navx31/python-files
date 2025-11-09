20c3b4a3d249a5d5d99c31f79eac887cc2950d99
navx31 <navjot17832@gmail.com>
2025-11-09 21:15:20 +0530

    Add elementwise operations examples for NumPy arrays

---
+++ numpy/operation.py
@@ -0,0 +1,19 @@
+import numpy as np
+x = np.array([2, 4, 6])
+y = np.array([1, 2, 3])
+
+# Elementwise operations
+x + y         
+x - y        
+x * y       
+x / y       
+x > 3        
+x ** 2     
+
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)         
+print("x > 3 =", x > 3)
+print("x ** 2 =", x ** 2)
+

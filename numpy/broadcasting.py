2e04009dd6147b31ea913b10a14cf1f7dcaed92e
navx31 <navjot17832@gmail.com>
2026-01-04 14:48:45 +0530

    yes

--- numpy/broadcasting.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-prices=np.array([10.5, 23.99, 5.75, 99.95])
-discount=10.0
-
-final_prices=prices - (prices*discount / 100)
-print("Original prices:", prices)
-print("Final prices after", discount, "% discount:", final_prices)
-

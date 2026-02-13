f564957f49c58fdc91aada67254ad7fc6ba3247b
navx31 <navjot17832@gmail.com>
2026-02-13 18:37:50 +0530

    new

---
+++ Exampredict.py
@@ -0,0 +1,47 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv('Exam_score.csv')
+df_dup=df.copy()
+df_dup.drop(['student_id','age'],axis=1,inplace=True)
+print(df_dup.head())
+print(df_dup.info())
+
+df_dup = pd.get_dummies(df_dup, columns=['gender','internet_access','sleep_quality','study_method','facility_rating','exam_difficulty','course'], dtype=int)
+
+mm=MinMaxScaler()
+df_dup['study_hours']=mm.fit_transform(df_dup['study_hours'].values.reshape(-1,1))
+df_dup['class_attendance']=mm.fit_transform(df_dup['class_attendance'].values.reshape(-1,1))
+df_dup['sleep_hours']=mm.fit_transform(df_dup['sleep_hours'].values.reshape(-1,1))
+
+X=df_dup.drop('exam_score',axis=1)
+y=df_dup['exam_score']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = LinearRegression()
+model.fit(X_train_scaled, y_train)
+y_pred = model.predict(X_test_scaled)
+
+print("\n--- Model Performance ---")
+print(f"R² Score: {r2_score(y_test, y_pred):.4f}")
+print(f"Mean Absolute Error: {mean_absolute_error(y_test, y_pred):,.2f}")
+print(f"Root Mean Squared Error: ${np.sqrt(mean_squared_error(y_test, y_pred)):,.2f}")  
+
+print("\n--- Actual vs Predicted ---")
+results = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+print(results.tail())
+
+
+
+
